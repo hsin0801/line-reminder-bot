1ee956d1aa1b41019e8f1084c60c71beddf526d0
hsin0801 <hsin0801@gmail.com>
2026-06-12 14:16:38 +0800

    Update app.py

--- app.py
+++ app.py
@@ -83,7 +83,7 @@ def webhook():
         elif text == "配件":
             reply_message(reply_token, [{
                 "type": "text",
-                "text": "📊 配件資料：\nhttps://docs.google.com/spreadsheets/d/1zFlrlMFGK4Dj_kzwZzAGZ5chnx1afMEm/edit?pli=1&gid=681928103#gid=681928103"
+                "text": "📊 配件資料：\nhttps://docs.google.com/spreadsheets/d/1SmYKmhIWleFA8YEGPb91mNoi5-J2CP3A/edit?usp=sharing&ouid=109189035277985438460&rtpof=true&sd=true"
             }])
 
         elif text == "組合價":
